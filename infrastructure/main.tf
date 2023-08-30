bbfe10b48e833b4ef2ebaff1609de399969cc60b
pedroo-csproj <pedrooctavio.dev@outlook.com>
2023-08-29 22:25:10 -0300

    container registry added

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,10 @@ resource "azurerm_resource_group" "rg-dnma" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+resource "azurerm_container_registry" "cr-dnma" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg-dnma.name
+  location            = azurerm_resource_group.rg-dnma.location
+  sku                 = var.container_registry_sku
+}
--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -1,2 +1,5 @@
 resource_group_name     = "#{ResourceGroupName}#"
 resource_group_location = "#{resourceGroupLocation}#"
+
+container_registry_name = "#{ContainerRegistryName}#"
+container_registry_sku  = "#{ContainerRegistrySKU}#"
