c22e4117618252e26500dca31180d7a28062b805
pedroo-csproj <pedrooctavio.dev@outlook.com>
2023-08-29 21:36:22 -0300

    terraform.tfvars created

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,10 @@ provider "azurerm" {
   features {}
 }
 
+variable "resource_group_name" {}
+variable "resource_group_location" {}
+
 resource "azurerm_resource_group" "rg-dnma" {
-  name     = "rg-dnma"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
---
+++ infrastructure/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name     = var.ResourceGroupName
+resource_group_location = var.ResourceGroupLocation
