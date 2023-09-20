b06f149d6012ab742c99dbc1fdb019606309cb16
pedroo-csproj <pedrooctavio.dev@outlook.com>
2023-09-20 16:36:52 -0300

    variables names updated

--- src/DotNETModernAPI.Application/Mappers/UserMappers.cs
+++ src/DotNETModernAPI.Application/Mappers/UserMappers.cs
@@ -9,13 +9,13 @@ public class UserMappers : Profile
     public UserMappers()
     {
         CreateMap<UserRolesView, ListUsersQueryResponse>()
-            .ForMember(luqr => luqr.Id, mo => mo.MapFrom(u => u.Id))
-            .ForMember(luqr => luqr.UserName, mo => mo.MapFrom(u => u.UserName))
-            .ForMember(luqr => luqr.Email, mo => mo.MapFrom(u => u.Email))
-            .ForMember(luqr => luqr.EmailConfirmed, mo => mo.MapFrom(u => u.EmailConfirmed));
+            .ForMember(luqr => luqr.Id, mo => mo.MapFrom(urv => urv.Id))
+            .ForMember(luqr => luqr.UserName, mo => mo.MapFrom(urv => urv.UserName))
+            .ForMember(luqr => luqr.Email, mo => mo.MapFrom(urv => urv.Email))
+            .ForMember(luqr => luqr.EmailConfirmed, mo => mo.MapFrom(urv => urv.EmailConfirmed));
 
         CreateMap<RoleVO, ListUsersRoleVO>()
-            .ForMember(x => x.Id, mo => mo.MapFrom(r => r.Id))
-            .ForMember(x => x.Name, mo => mo.MapFrom(r => r.Name));
+            .ForMember(lurvo => lurvo.Id, mo => mo.MapFrom(rv => rv.Id))
+            .ForMember(lurvo => lurvo.Name, mo => mo.MapFrom(rv => rv.Name));
     }
 }
