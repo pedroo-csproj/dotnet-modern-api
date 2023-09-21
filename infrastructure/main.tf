7786f396d1b9b8b33df094963047622b54a70df7
pedroo-csproj <pedrooctavio.dev@outlook.com>
2023-09-21 00:40:36 -0300

    id updated

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_linux_web_app" "lwa-dnma" {
 }
 
 resource "azurerm_role_assignment" "ra" {
-  principal_id                     = azurerm_linux_web_app.lwa-dnma.identity[0].object_id
+  principal_id                     = azurerm_linux_web_app.lwa-dnma.identity[0].principal_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.cr-dnma.id
   skip_service_principal_aad_check = true
