a64261fa3568258635b82d7f7e0fe0d7d1e69e45
pedroo-csproj <pedrooctavio.dev@outlook.com>
2023-11-27 17:21:56 -0300

    integration tests project

--- src/DotNETModernAPI.Presentation/Program.cs
+++ src/DotNETModernAPI.Presentation/Program.cs
@@ -4,31 +4,40 @@ using DotNETModernAPI.Presentation.Configurations;
 using DotNETModernAPI.Presentation.Policies;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace DotNETModernAPI.Presentation;
 
-builder.Services.AddDependencies(builder.Configuration);
-builder.Services.AddSwagger(builder.Configuration);
-builder.Services.AddJwtConfiguration(builder.Configuration);
-builder.Services.AddPolicies(builder.Configuration);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    using var scope = app.Services.CreateScope();
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-    var applicationDataContext = scope.ServiceProvider.GetRequiredService<ApplicationDataContext>();
+        builder.Services.AddDependencies(builder.Configuration);
+        builder.Services.AddSwagger(builder.Configuration);
+        builder.Services.AddJwtConfiguration(builder.Configuration);
+        builder.Services.AddPolicies(builder.Configuration);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-    await applicationDataContext.Database.MigrateAsync();
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            using var scope = app.Services.CreateScope();
+
+            var applicationDataContext = scope.ServiceProvider.GetRequiredService<ApplicationDataContext>();
+
+            await applicationDataContext.Database.MigrateAsync();
+        }
+
+        app.UseSwagger(builder.Configuration);
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseSwagger(builder.Configuration);
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
---
+++ src/tests/DotNETModernAPI.Presentation.Tests/TestingWebApplicationFactory.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DotNETModernAPI.Presentation.Tests;
+
+public class TestingWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class { }
